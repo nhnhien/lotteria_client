df3eb1c1437ab7a057fa7322a1cf77e098588f12
nhnhien <nguyenhaonhien91@gmail.com>
2024-12-02 23:45:24 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+	return (
+		<>
+			<div className="text-red-500">Hello</div>
+		</>
+	);
+}
+
+export default App;
---
+++ src/pages/order/OrderScreen.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const OrderScreen = () => {
+	return <div>OrderScreen</div>;
+};
